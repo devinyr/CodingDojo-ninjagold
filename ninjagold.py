0f57237743fe85dc51f5d703088c29b979bcced4
Monty <monty@dnmacbook.hsd1.wa.comcast.net>
2015-11-06 14:01:09 -0800

    Finished Activities Text Area

--- ninjagold.py
+++ ninjagold.py
@@ -10,31 +10,36 @@ app.secret_key = "SecretKey"
 def index():
     try:
         session['yourGold']
-        session['fromWhere'] = ''
-        session['update'] = 0
+        session['fromWhere']
+        session['update']
+        session['timeStamp']
+        session['activities']
     except:
         session['yourGold'] = 0
         session['fromWhere'] = ''
         session['update'] = 0
+        session['timeStamp'] = ''
+        session['activities'] = ['Start']
     return render_template('index.html') 
 
 @app.route('/process', methods=['POST'])
 def process():
+    
     if request.form['building'] == 'farm':
         val = random.randrange(10,20)
         session['update'] = val
         session['yourGold'] += val
-        session['fromWhere'] ='Farm'
+        session['fromWhere'] ='farm'
     elif request.form['building'] == 'cave':
         val = random.randrange(5,10)
         session['update'] = val
         session['yourGold'] += val
-        session['fromWhere'] ='Cave'
+        session['fromWhere'] ='cave'
     elif request.form['building'] == 'house':
         val = random.randrange(2,5)
         print val
         session['yourGold'] += val
-        session['fromWhere'] ='House'
+        session['fromWhere'] ='house'
     elif request.form['building'] == 'casino':
         i = random.randrange(0,1)
         print i
@@ -46,11 +51,20 @@ def process():
             val = random.randrange(0,50)
             session['update'] = -1 * val
             session['yourGold'] -= val
-        session['fromWhere'] ='Casino'    
+        session['fromWhere'] ='casino'    
 
     ts = time.time()
     st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-    print st
+    session['timeStamp']  = st
+    
+    actv = ''
+    if  session['fromWhere'] == 'farm' or session['fromWhere'] == 'cave' or  session['fromWhere'] == 'house':            
+        actv = 'Earned' + str(session['update']) + 'gold from ' + session['fromWhere'] + '!' +  session['timeStamp']        
+    else:
+        actv = 'Entered a casino and lost ' + str(session['update']) + ' gold ... Ouch... ' + session['timeStamp']        
+    
+    session['activities'].append(actv)
+
 
     return redirect('/')
 
